2201a75133dd56ef7eade4ba077f045aaa849d73
Rayudu <venkata.rayudu@softwareone.com>
2026-03-30 19:58:54 +0530

    add vpc and s3 modules

---
+++ modules/s3/main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "this" {
+  bucket = "${var.environment}-${var.bucket_suffix}"
+
+  tags = {
+    Name        = "${var.environment}-${var.bucket_suffix}"
+    Environment = var.environment
+  }
+}
