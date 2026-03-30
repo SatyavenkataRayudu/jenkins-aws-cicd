320cab7b095355fba5f4ee229fa91e3492acba72
SatyavenkataRayudu <118960346+SatyavenkataRayudu@users.noreply.github.com>
2026-03-30 20:54:44 +0530

    Enable lockfile usage for S3 backend

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "s3-backend-jaggampeta"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
