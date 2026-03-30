0fca5b2afe50be041df453b8cc02c6a97598a40a
SatyavenkataRayudu <118960346+SatyavenkataRayudu@users.noreply.github.com>
2026-03-30 21:03:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "s3-backend-jaggampeta"
-    key            = "terraform.tfstate"
+    key            = "path/to/my/key"
     region         = "ap-south-1"
-    use_lockfile   = true
-    encrypt        = true
   }
 }
